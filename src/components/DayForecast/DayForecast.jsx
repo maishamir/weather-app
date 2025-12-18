d115ea092cbfea23d177df271460683a461e83a1
Maisha Mir <maisha.mir2014@gmail.com>
2025-12-18 14:10:27 -0800

    cleaned up BEM naming

--- src/components/DayForecast/DayForecast.jsx
+++ src/components/DayForecast/DayForecast.jsx
@@ -4,30 +4,44 @@ import sunImg from "/images/icon-sunny.webp";
 
 function DayForecast() {
     return (
-        <section className="dayForecast">
-            <div className="parent">
-                <div className='div1'>
-                    Berlin, Germany
-                    <small>Tuesday, Aug 5, 2025</small>
-                    <img src={sunImg} alt="Sunny" className='dayForecast__icon' />
-                    <h1>68°</h1>
+        <section className="day-forecast">
+            <div className="day-forecast__container">
+
+                <div className="day-forecast__main">
+                    <p className="day-forecast__location">Berlin, Germany</p>
+                    <small className="day-forecast__date">Tuesday, Aug 5, 2025</small>
+
+                    <img
+                        src={sunImg}
+                        alt="Sunny"
+                        className="day-forecast__icon"
+                    />
+
+                    <h1 className="day-forecast__temperature">20°</h1>
                 </div>
-                <div className='div2'>
-                    <small>Feels like </small>
-                    <p>64°</p>
+
+                <div className="day-forecast__stats">
+                    <div className="day-forecast__stat">
+                        <small className="day-forecast__stat-label">Feels like</small>
+                        <p className="day-forecast__stat-value">18°</p>
+                    </div>
+
+                    <div className="day-forecast__stat">
+                        <small className="day-forecast__stat-label">Humidity</small>
+                        <p className="day-forecast__stat-value">46%</p>
+                    </div>
+
+                    <div className="day-forecast__stat">
+                        <small className="day-forecast__stat-label">Wind</small>
+                        <p className="day-forecast__stat-value">14 km/h</p>
+                    </div>
+
+                    <div className="day-forecast__stat">
+                        <small className="day-forecast__stat-label">Precipitation</small>
+                        <p className="day-forecast__stat-value">0 in</p>
+                    </div>
                 </div>
-                <div className='div3'>
-                    <small>Humidity</small>
-                    <p>46%</p>
-                </div>
-                <div className='div4'>
-                    <small>Wind</small>
-                    <p>9 mph</p>
-                </div>
-                <div className='div5'>
-                    <small>Precipitation</small>
-                    <p>0 in</p>
-                </div>
+
             </div>
         </section>
     )
